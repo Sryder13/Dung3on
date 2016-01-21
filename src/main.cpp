052bea4e8b928fd86b4e08a3ed5c505e05390faf
Sean Ryder <sryder13@gmail.com>
2016-01-21 16:04:35 +0000

    CMake base setup and test

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "config.hpp"
+
+int main(int argc, char *argv[])
+{
+   std::cout << "Hello World! This is Version " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+   return 0;
+}
