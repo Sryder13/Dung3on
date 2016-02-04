6eb0a88a1e590dcaac4b83de2609359572449a99
Sean Ryder <sryder13@gmail.com>
2016-02-04 15:48:52 +0000

    OpenGL with GLEW test. Removed SDL2_Image from CMakeLists.txt until it is needed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 #include<SDL.h>
+#include<GL/glew.h>
+#include<SDL_opengl.h>
 #include "config.hpp"
 
 // TODO (sean): Create settings to change screen resolution instead of defines
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-bool init();
-void close();
+bool init(); // Initialise SDL and base OpenGL
+bool initGL(); // Initialise matrices and clear Colour for OpenGL
+void render(); // Render the screen
+void close(); // Run before close to clear variables
 
 // Window we render to
 SDL_Window *window = NULL;
-// Surface containing the window
-SDL_Surface *screenSurface = NULL;
+// OpenGL context
+SDL_GLContext context = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -52,6 +56,10 @@ int main(int argc, char *argv[])
 					}
 				}
 			}
+
+			render();
+
+			SDL_GL_SwapWindow(window);
 		}
 	}
 
@@ -70,8 +78,12 @@ bool init()
 	}
 	else
 	{
+		//Use OpenGL 2.1
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
 		// Create Window
-		window = SDL_CreateWindow("Dung3on", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("Dung3on", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL);
 
 		if (window == NULL)
 		{
@@ -80,18 +92,93 @@ bool init()
 		}
 		else
 		{
-			// Get Window Surface
-            screenSurface = SDL_GetWindowSurface(window);
+			// Create GL context
+			context = SDL_GL_CreateContext(window);
 
-			// Fill surface with white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			if (context == NULL)
+			{
+				std::cout << "SDL failed to create OpenGL context. SDL error: " << SDL_GetError() << std::endl;
+				return false;
+			}
+			else
+			{
+				// Initialise GLEW
+				GLenum glewError = glewInit();
+				if (glewError != GLEW_OK)
+				{
+					std::cout << "Unable to initialise GLEW. GLEW error: " << glewGetErrorString(glewError) << std::endl;
+					return false;
+				}
 
-			// Update Window
-            SDL_UpdateWindowSurface(window);
+				//Use Vsync
+				if( SDL_GL_SetSwapInterval( 1 ) < 0 )
+				{
+					std::cout << "SDL failed to set vsync. SDL error: " << SDL_GetError() << std::endl;
+				}
+
+				// Initialise OpenGL
+				if (!initGL())
+				{
+					std::cout << "Unable to initialise OpenGL" << std::endl;
+					return false;
+				}
+			}
 		}
 	}
 
 	return true;
+}
+
+bool initGL()
+{
+	GLenum glError = GL_NO_ERROR;
+
+	// Initialise projection matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glError = glGetError();
+	if (glError != GL_NO_ERROR)
+	{
+		std::cout << "OpenGL Error: " << gluErrorString(glError) << std::endl;
+		return false;
+	}
+
+	// Initialise modelview matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glError = glGetError();
+	if (glError != GL_NO_ERROR)
+	{
+		std::cout << "OpenGL Error: " << gluErrorString(glError) << std::endl;
+		return false;
+	}
+
+	// Set clear colour
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glError = glGetError();
+	if (glError != GL_NO_ERROR)
+	{
+		std::cout << "OpenGL Error: " << gluErrorString(glError) << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void render()
+{
+	// Clear screen before rendering next frame
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// TODO (sean): This is obviously deprecated, proper rendering should be done
+	glBegin(GL_TRIANGLES);
+		glVertex2f( -0.5f, -0.5f );
+		glVertex2f( 0.5f, -0.5f );
+		glVertex2f( 0.0f, 0.5f );
+	glEnd();
 }
 
 void close()
@@ -99,7 +186,7 @@ void close()
 	//Destroy window
 	SDL_DestroyWindow(window);
 	window = NULL;
-	screenSurface = NULL;
+	context = NULL;
 
 	//Quit SDL subsystems
 	SDL_Quit();
